0ce5b6c12e879a0f3083241aa03c4fa6e489e4aa
Bogdan <bogdan9110@gmail.com>
2023-01-19 13:37:52 +0200

    FB pixel analityc settings

--- scripts/meta-metrics.js
+++ scripts/meta-metrics.js
@@ -1,27 +1,39 @@
 const playMarketArr = document.querySelectorAll('.playmarket');
 const appStoreArr = document.querySelectorAll('.appstore');
-const position = {
-	1: 'top',
-	2: 'middle',
-	3: 'bottom',
-};
+// const position = {
+// 	1: 'top',
+// 	2: 'middle',
+// 	3: 'bottom',
+// };
 
-const playMarketHandler = (num) => {
-	let positionItem = position[`${num + 1}`];
-	fbq('send', 'Subscribe', 'gplay', 'click', positionItem);
+// const playMarketHandler = (num) => {
+// 	let positionItem = position[`${num + 1}`];
+// 	fbq('send', 'Subscribe', 'gplay', 'click', positionItem);
+// 	// console.log(positionItem);
+// }
+
+// const appStoreHandler = (num) => {
+// 	let positionItem = position[`${num + 1}`];
+// 	fbq('send', 'Subscribe', 'ios', 'click', positionItem);
+// 	// console.log(positionItem);
+// }
+
+const playMarketHandler = (e) => {
+	fbq('track', 'Purchase');
 	// console.log(positionItem);
 }
 
-const appStoreHandler = (num) => {
-	let positionItem = position[`${num + 1}`];
-	fbq('send', 'Subscribe', 'ios', 'click', positionItem);
+const appStoreHandler = (e) => {
+
+	fbq('track', 'Lead');
 	// console.log(positionItem);
 }
+
 
 playMarketArr.forEach((el, i) => {
-	el.addEventListener('click', playMarketHandler(i));
+	el.addEventListener('click', playMarketHandler);
 });
 
 appStoreArr.forEach((el, i) => {
-	el.addEventListener('click', appStoreHandler(i));
+	el.addEventListener('click', appStoreHandler);
 });
